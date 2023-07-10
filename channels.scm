ea7137902b46dc1aff29f6af7771d565501e1c8b
nph278 <carllegrone@protonmail.com>
2023-07-09 21:23:50 -0400

    no nonguix

--- channels.scm
+++ channels.scm
@@ -1,8 +1,4 @@
 (list (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
-        (commit "a033c0cf992d1b35b8dfe3424451ca96bee7c5d4"))
-      (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (commit "15e5924de8696cc4a19a8f508501d07f799650e1")))
+        (commit "a033c0cf992d1b35b8dfe3424451ca96bee7c5d4")))
