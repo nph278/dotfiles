009a97d30e6e2d6aebc1979b2ac61a3336b8ca59
nph278 <carllegrone@protonmail.com>
2023-11-27 17:52:52 -0500

    channels and channels-lock

--- channels.scm
+++ channels.scm
@@ -1,4 +1,5 @@
-(list (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (commit "9f67681db15d22f7ede5488315d52b78e357343e")))
+(use-modules (guix ci))
+
+(list (channel-with-substitutes-available
+       %default-guix-channel
+       "https://ci.guix.gnu.org"))
