4c2c28721420ca669acd9410ff0611b14c537f93
nph278 <carllegrone@protonmail.com>
2025-10-12 20:56:23 -0400

    whoops

--- extra/add_ethernet.sh
+++ extra/add_ethernet.sh
@@ -1,5 +1,5 @@
-#!/bin/sudo su
+#!/bin/sh
 
-ip route add 192.168.4.1 dev enp0s31f6 metric 500
-ip route add default via 192.168.4.1 dev enp0s31f6 metric 500
-ip route add 192.168.4.0/22 dev enp0s31f6 metric 500
+sudo ip route add 192.168.4.1 dev enp0s31f6 metric 500
+sudo ip route add default via 192.168.4.1 dev enp0s31f6 metric 500
+sudo ip route add 192.168.4.0/22 dev enp0s31f6 metric 500
