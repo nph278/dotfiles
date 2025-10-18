f1a17afe6b516e421022900aec5ce445364df911
nph278 <carllegrone@protonmail.com>
2025-10-18 15:51:39 -0400

    network device on status bar

--- .config/sway_status.sh
+++ .config/sway_status.sh
@@ -4,4 +4,6 @@ date=$(date +'%Y-%m-%d %I:%M:%S %p')
 
 batt=$(upower -i "$(upower -e | grep battery)" | grep percentage | awk '{print $2}' || echo "PC")
 
-echo $batt " | " $date
+netdev=$(ip r get 1.1.1.1 | grep 'dev ' | sed 's/.*dev \([^ ]*\).*/\1/')
+
+echo $batt " | " $netdev " | " $date
